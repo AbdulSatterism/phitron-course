1d2275ddf6c12457e6aff0200b901b284c0fe19f
AbdulSatterism <abdulsatter.ism@gmail.com>
2025-08-16 23:15:38 +0600

    fixed_password problem solved

---
+++ c-programming/fixed_password.c
@@ -0,0 +1,21 @@
+// EOF
+
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    while (scanf("%d", &n) != EOF)
+    {
+        if(n==1999){
+            printf("Correct\n");
+            break;
+        }
+        else{
+            printf("Wrong\n");
+        }
+    }
+
+    return 0;
+    
+}

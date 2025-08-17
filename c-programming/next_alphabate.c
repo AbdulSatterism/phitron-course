c702a0e22221d96c2647fc3aaf6a6d6dcd367019
AbdulSatterism <abdulsatter.ism@gmail.com>
2025-08-17 23:15:28 +0600

    next alphabate

--- c-programming/next_alphabate.c
+++ c-programming/next_alphabate.c
@@ -1,27 +1,69 @@
 
 
+// #include <stdio.h>
+
+// int main() {
+//     char ch, next;
+
+//     printf("Enter a character: ");
+//     scanf(" %c", &ch);   // note the space before %c to ignore whitespace
+
+//     if (ch >= 'A' && ch <= 'Z') {
+//         // Uppercase case
+//         next = 'A' + ( (ch - 'A' + 1) % 26 );
+//     }
+//     else if (ch >= 'a' && ch <= 'z') {
+//         // Lowercase case
+//         next = 'a' + ( (ch - 'a' + 1) % 26 );
+//     }
+//     else {
+//         printf("Not an alphabet!\n");
+//         return 0;
+//     }
+
+//     printf("Next alphabet: %c\n", next);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
-int main() {
+int main()
+{
+
     char ch, next;
 
-    printf("Enter a character: ");
-    scanf(" %c", &ch);   // note the space before %c to ignore whitespace
+    printf("Enter a alphabate : ");
+    scanf("%c", &ch);
 
-    if (ch >= 'A' && ch <= 'Z') {
-        // Uppercase case
-        next = 'A' + ( (ch - 'A' + 1) % 26 );
-    } 
-    else if (ch >= 'a' && ch <= 'z') {
-        // Lowercase case
-        next = 'a' + ( (ch - 'a' + 1) % 26 );
-    } 
-    else {
-        printf("Not an alphabet!\n");
-        return 0;
+    if (ch >= 'A' && ch <= 'A')
+    {
+        if (ch == 'Z')
+        {
+            next = 'A';
+        }
+        else
+        {
+            next = ch + 1;
+        }
     }
 
-    printf("Next alphabet: %c\n", next);
+    else if (ch >= 'a' && ch <= 'z')
+    {
+        if (ch == 'z')
+        {
+            next = 'a';
+        }
+        else
+        {
+            next = ch + 1;
+        }
+    }
+    else{
+        printf("not alphabate");
+    }
+
+    printf("%c", next);
 
     return 0;
 }

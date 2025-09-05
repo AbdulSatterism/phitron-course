95fe7d751b02f90c9d9881acd2bdef1f72f3ef1a
AbdulSatterism <abdulsatter.ism@gmail.com>
2025-09-06 00:19:41 +0600

    sum two number in a array with check target value

---
+++ c-programming/sum-two-x.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int n;
+
+    printf("enter array size : ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    int flag=0;
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    int x;
+    scanf("%d",&x);
+
+    for (int i = 0; i < n-i; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if( a[i], a[j] == x){
+                flag=1;
+            }
+        }
+        
+    }
+    
+    if (flag==1){
+        printf("yes");
+    }else{
+        printf("No");
+    }
+
+
+    return 0;
+    
+}
